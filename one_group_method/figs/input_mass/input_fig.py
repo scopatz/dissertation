25e36263e07e2a3a3db8dddbe1e76366f7e30aca
Anthony Scopatz <ascopatz@enthought.com>
2011-07-30 18:21:05 -0500

    Added some LWR Needed figs

--- one_group_method/figs/input_mass/input_fig.py
+++ one_group_method/figs/input_mass/input_fig.py
@@ -6,22 +6,25 @@ from matplotlib import rc
 rc('text', usetex=True)
 rc('font', family='roman')
 
-
+linestyles = ['k-', 'r--', 'bo-', 'gs-']
 
 def read_data(fname):
     arr = np.genfromtxt(fname, skip_header=1, usecols=(np.arange(10)*2+1), dtype=float)
     return arr
 
 
-def graph_four(xset, yset, xsetlabel, ysetlabel, figname = ""):
-    plt.plot(xset, yset[0], 'k-', label=ysetlabel[0])
-    plt.plot(xset, yset[1], 'r--', label=ysetlabel[1])
-    plt.plot(xset, yset[2], 'bo-', label=ysetlabel[2])
-    plt.plot(xset, yset[3], 'gs-', label=ysetlabel[3])
+def graph_data(xset, yset, xsetlabel, ysetlabel, figname="", yaxislabel=None, ax=None):
+    for i, ydata in enumerate(yset):
+        plt.plot(xset, ydata, linestyles[i], label=ysetlabel[i])
 
     plt.legend(loc=0)
 
     plt.xlabel(xsetlabel)
+    if yaxislabel is not None:
+        plt.ylabel(yaxislabel)
+
+    if ax is not None:
+        plt.axis(ax)
 
     plt.savefig(figname + ".eps")
     #plt.savefig(figname + ".png")
@@ -36,4 +39,10 @@ if __name__ == "__main__":
     d = d[[1,2,3,4]]
 
     ylabels = ["DU Input Mass", "LWR-TRU Input Mass", "FR-U Input Mass", "FR-TRU Input Mass"]
-    graph_four(np.arange(1, 11), d, "Cycle Number", ylabels, figname="input_figs")
+    graph_data(np.arange(1, 11), d, "Cycle Number", ylabels, figname="input_figs")
+
+
+    d = read_data("MultiCycleTotal.txt")
+    d = d[[8]]
+    graph_data(np.arange(1, 11), d, "Cycle Number", ["LWR UF Needed [kg]"], 
+               "lwr_uf_needed", "LWR UF Mass Needed [kg]", [1, 10, 0, 26])
